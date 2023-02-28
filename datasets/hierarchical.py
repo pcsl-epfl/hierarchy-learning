87ef441f0f2039bca4e1112413f2d58954c94a2a
Leonardo Petrini <32774703+leonardopetrini@users.noreply.github.com>
2023-02-28 10:52:03 +0100

    deprecation: use torch.div instead of //

--- datasets/hierarchical.py
+++ datasets/hierarchical.py
@@ -8,6 +8,7 @@ import numpy as np
 
 from .utils import dec2bin
 
+
 def hierarchical_features(num_features, num_layers, m, num_classes, seed=0):
     """
     Build hierarchy of features.
@@ -27,7 +28,7 @@ def hierarchical_features(num_features, num_layers, m, num_classes, seed=0):
         # new_features = list(combinations(range(num_features), 2))
         new_features = list(product(range(num_features), range(num_features)))
         assert (
-            len(new_features) >= m * num_layer_features
+                len(new_features) >= m * num_layer_features
         ), "Not enough features to choose from!!"
         random.shuffle(new_features)
         new_features = new_features[: m * num_layer_features]
@@ -47,6 +48,7 @@ def hierarchical_features(num_features, num_layers, m, num_classes, seed=0):
         features.append(new_features)
     return features
 
+
 def features_to_data(samples_indices, features, m, num_classes, num_layers, seed=0, seed_reset_layer=42):
     """
     Build hierarchical dataset from features hierarchy.
@@ -60,13 +62,13 @@ def features_to_data(samples_indices, features, m, num_classes, num_layers, seed
     :param seed_reset_layer: layer from which to randomize the choice of semantically equivalent subfeatures (for stability measurements)
     :return: dataset {x, y}
     """
-    
+
     Pmax = m ** (2 ** num_layers - 1) * num_classes
 
-    x = features[-1].reshape(num_classes, *sum([(m, 2) for _ in range(num_layers)], ())) # [nc, m, 2, m, 2, ...]
-    
+    x = features[-1].reshape(num_classes, *sum([(m, 2) for _ in range(num_layers)], ()))  # [nc, m, 2, m, 2, ...]
+
     groups_size = Pmax // num_classes
-    y = samples_indices // groups_size
+    y = samples_indices.div(groups_size, rounding_mode='floor')
     samples_indices = samples_indices % groups_size
 
     indices = []
@@ -77,40 +79,41 @@ def features_to_data(samples_indices, features, m, num_classes, num_layers, seed
             # Repeat is there such that higher level features are chosen consistently for a give data-point
             left_right = (
                 torch.arange(2)[None]
-                .repeat(2 ** (num_layers - 2), 1)
-                .reshape(2 ** (num_layers - l - 1), -1)
-                .t()
-                .flatten()
+                    .repeat(2 ** (num_layers - 2), 1)
+                    .reshape(2 ** (num_layers - l - 1), -1)
+                    .t()
+                    .flatten()
             )
             left_right = left_right[None].repeat(len(samples_indices), 1)
-            
+
             indices.append(left_right)
 
         if l >= seed_reset_layer:
             np.random.seed(seed + 42 + l)
             perm = torch.randperm(len(samples_indices))
             samples_indices = samples_indices[perm]
-            
+
         groups_size //= m ** (2 ** l)
-        layer_indices = samples_indices // groups_size
-        
-        rules = number2base(layer_indices, m, string_length=2**l)
+        layer_indices = samples_indices.div(groups_size, rounding_mode='floor')
+
+        rules = number2base(layer_indices, m, string_length=2 ** l)
         rules = (
             rules[:, None]
-            .repeat(1, 2 ** (num_layers - l - 1), 1)
-            .permute(0, 2, 1)
-            .flatten(1)            
+                .repeat(1, 2 ** (num_layers - l - 1), 1)
+                .permute(0, 2, 1)
+                .flatten(1)
         )
-    
+
         indices.append(rules)
-        
+
         samples_indices = samples_indices % groups_size
-        
+
     yi = y[:, None].repeat(1, 2 ** (num_layers - 1))
 
     x = x[tuple([yi, *indices])].flatten(1)
 
     return x, y
+
 
 class HierarchicalDataset(Dataset):
     """
@@ -118,20 +121,20 @@ class HierarchicalDataset(Dataset):
     """
 
     def __init__(
-        self,
-        num_features=8,
-        m=2,  # features multiplicity
-        num_layers=2,
-        num_classes=2,
-        seed=0,
-        max_dataset_size=None,
-        seed_traintest_split=0,
-        train=True,
-        input_format='onehot',
-        whitening=0,
-        transform=None,
-        testsize=-1,
-        seed_reset_layer=42,
+            self,
+            num_features=8,
+            m=2,  # features multiplicity
+            num_layers=2,
+            num_classes=2,
+            seed=0,
+            max_dataset_size=None,
+            seed_traintest_split=0,
+            train=True,
+            input_format='onehot',
+            whitening=0,
+            transform=None,
+            testsize=-1,
+            seed_reset_layer=42,
     ):
         assert testsize or train, "testsize must be larger than zero when generating a test set!"
         torch.manual_seed(seed)
@@ -139,34 +142,34 @@ class HierarchicalDataset(Dataset):
         self.m = m  # features multiplicity
         self.num_layers = num_layers
         self.num_classes = num_classes
-        
+
         features = hierarchical_features(
             num_features, num_layers, m, num_classes, seed=seed
         )
-        
+
         Pmax = m ** (2 ** num_layers - 1) * num_classes
         assert Pmax < 1e19, "Pmax cannot be represented with int64!! Parameters too large! Please open a github issue if you need a solution."
         if max_dataset_size is None:
             max_dataset_size = Pmax
         if testsize == -1:
             testsize = min(max_dataset_size // 5, 20000)
-            
+
         g = torch.Generator()
         g.manual_seed(seed_traintest_split)
 
-        if Pmax < 5e6: # there is a crossover in computational time of the two sampling methods around this value of Pmax
+        if Pmax < 5e6:  # there is a crossover in computational time of the two sampling methods around this value of Pmax
             samples_indices = torch.randperm(Pmax, generator=g)[:max_dataset_size]
         else:
             samples_indices = torch.randint(Pmax, (2 * max_dataset_size,), generator=g)
             samples_indices = torch.unique(samples_indices)
             perm = torch.randperm(len(samples_indices), generator=g)[:max_dataset_size]
             samples_indices = samples_indices[perm]
-    
+
         if train and testsize:
             samples_indices = samples_indices[:-testsize]
         else:
             samples_indices = samples_indices[-testsize:]
-                
+
         self.x, self.targets = features_to_data(
             samples_indices, features, m, num_classes, num_layers, seed=seed, seed_reset_layer=seed_reset_layer
         )
@@ -221,6 +224,7 @@ class HierarchicalDataset(Dataset):
 
         return x, y
 
+
 def pairs_to_num(xi, n):
     """
         Convert one long input with n-features encoding to n^2 pairs encoding.
@@ -231,6 +235,7 @@ def pairs_to_num(xi, n):
         xin[ii // 2] += xii * n ** (1 - ii % 2)
     return xin
 
+
 def pairing_features(x, n):
     """
         Batch of inputs from n to n^2 encoding.
@@ -240,11 +245,12 @@ def pairing_features(x, n):
         xn[i] = pairs_to_num(xi, n)
     return xn
 
+
 def number2base(numbers, base, string_length=None):
     digits = []
     while numbers.sum():
         digits.append(numbers % base)
-        numbers //= base
+        numbers = numbers.div(base, rounding_mode='floor')
     if string_length:
         assert len(digits) <= string_length, "String length required is too small to represent numbers!"
         digits += [torch.zeros(len(numbers), dtype=int)] * (string_length - len(digits))
